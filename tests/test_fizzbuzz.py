0e6275d89ddc7d1fbd5dbde0b5d7f1cafb5a2e1e
Mateo Lamthi <mateo.lamthi@outlook.com>
2024-10-14 11:45:37 +0200

    Add failing test for regular numbers

---
+++ tests/test_fizzbuzz.py
@@ -0,0 +1,9 @@
+import unittest
+
+class TestFizzBuzz(unittest.TestCase):
+    def test_regular_number(self):
+        self.assertEqual(fizzbuzz(1), "1")
+        self.assertEqual(fizzbuzz(2), "2")
+
+if __name__ == '__main__':
+    unittest.main()
