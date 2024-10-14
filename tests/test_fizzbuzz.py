dc31ea901998db46e5b326f0ec33124a301196a8
Mateo Lamthi <mateo.lamthi@outlook.com>
2024-10-14 11:49:00 +0200

    Add tests for multiples of 3 and 5

--- tests/test_fizzbuzz.py
+++ tests/test_fizzbuzz.py
@@ -6,5 +6,16 @@ class TestFizzBuzz(unittest.TestCase):
         self.assertEqual(fizzbuzz(1), "1")
         self.assertEqual(fizzbuzz(2), "2")
 
+    def test_fizz(self):
+        self.assertEqual(fizzbuzz(3), "Fizz")
+        self.assertEqual(fizzbuzz(6), "Fizz")
+
+    def test_buzz(self):
+        self.assertEqual(fizzbuzz(5), "Buzz")
+        self.assertEqual(fizzbuzz(10), "Buzz")
+
+    def test_fizzbuzz(self):
+        self.assertEqual(fizzbuzz(15), "FizzBuzz")
+        self.assertEqual(fizzbuzz(30), "FizzBuzz")
 if __name__ == '__main__':
     unittest.main()
